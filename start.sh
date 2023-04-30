12ed3b8d3dce835d3b8115b98d27af7b7de39e0c
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-04-30 16:05:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hintpirox/Shortner.git /Shortner
+  git clone https://github.com/hintpirox/tgraph.git /tgraph
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Shortner
+  git clone $UPSTREAM_REPO /tgraph
 fi
-cd /Shortner
+cd /tgraph
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
