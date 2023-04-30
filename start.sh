c42d26a6f1dc29a8bf92c8578ea31054c15a884a
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-04-30 16:26:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hintpirox/tgraph.git/tgraph
+  git clone https://github.com/hintpirox/tgraph.git /tgraph
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /tgraph
